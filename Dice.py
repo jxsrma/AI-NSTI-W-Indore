aad738b0e4c25c7543f775e9b34bbe9eb5e7ddd3
Jash <jashsharma619@gmail.com>
2025-04-30 09:56:30 +0530

    Test

---
+++ Dice.py
@@ -0,0 +1,24 @@
+import random
+
+option = 0
+while option != 2:
+    int(input(
+        """\n
+1. Roll a Dice/s \n
+2. Exit
+"""))
+    result = 0
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    print("Dice 1: ", dice1)
+    print("Dice 2: ", dice2)
+    result = dice1 + dice2
+    print("Dice Output: ", result)
+    """
+    
+    
+    Hello Everone
+    
+    
+    
+    """
